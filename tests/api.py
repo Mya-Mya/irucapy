d2000a52d62897885ba6f19d5e38ef45854b5765
Mya-Mya <interleukin413@gmail.com>
2022-04-26 22:32:55 +0900

    Update api.py

--- tests/api.py
+++ tests/api.py
@@ -1,6 +1,7 @@
 from mytesttool import testerfunc
 import irucapy
 
+
 @testerfunc
 def room():
     print("Valid Room Json Text")
@@ -29,6 +30,7 @@ def room():
         '{"additional":"additional","id":1,"code":"81263a2b-1bdd-49a4-bbc0-7a59639fcb28","name":"Mya-Mya Room","note":"This room does not exist.","statuses":["A","B"],"created_at":"2021-10-01T01:00:00.000Z","updated_at":"2022-04-01T16:00:00.000Z","status_updated_at":"2022-05-01T22:00:00.000Z"}'
     obj = irucapy.api.room.from_json_maybe(excessive_room_json_text)
     assert obj is None
+
 
 @testerfunc
 def member():
@@ -59,5 +61,50 @@ def member():
     obj = irucapy.api.member.from_json_maybe(excessive_member_json_text)
     assert obj is None
 
+
+@testerfunc
+def members():
+    print("Valid Members Json Text")
+    valid_members_json_text =\
+        '['\
+        '{"id":10000,"room_id":1,"name":"Mya-Mya","status":"B","message":"I do not exist.","created_at":"2022-02-10T03:10:00.000Z","updated_at":"2022-03-20T16:00:00.000Z","position":4},'\
+        '{"id":10001,"room_id":1,"name":"Nya-Nya","status":"A","message":"I do not not exist.","created_at":"2022-02-10T04:10:00.000Z","updated_at":"2022-03-20T17:00:00.000Z","position":5},'\
+        '{"id":10002,"room_id":1,"name":"Kya-Kya","status":"A","message":"I do not not not exist.","created_at":"2022-02-10T04:12:00.000Z","updated_at":"2022-03-20T18:00:00.000Z","position":6}'\
+        ']'
+    obj = irucapy.api.members.from_json_maybe(valid_members_json_text)
+    assert obj == [
+        irucapy.api.member.Member(
+            id=10000,
+            room_id=1,
+            name="Mya-Mya",
+            status="B",
+            message="I do not exist.",
+            created_at="2022-02-10T03:10:00.000Z",
+            updated_at="2022-03-20T16:00:00.000Z",
+            position=4
+        ),
+        irucapy.api.member.Member(
+            id=10001,
+            room_id=1,
+            name="Nya-Nya",
+            status="A",
+            message="I do not not exist.",
+            created_at="2022-02-10T04:10:00.000Z",
+            updated_at="2022-03-20T17:00:00.000Z",
+            position=5
+        ),
+        irucapy.api.member.Member(
+            id=10002,
+            room_id=1,
+            name="Kya-Kya",
+            status="A",
+            message="I do not not not exist.",
+            created_at="2022-02-10T04:12:00.000Z",
+            updated_at="2022-03-20T18:00:00.000Z",
+            position=6
+        )
+    ]
+
 room()
 member()
+members()
