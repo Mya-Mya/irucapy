6c754738984fbecf056442e37f9cf4714b8867db
Mya-Mya <interleukin413@gmail.com>
2022-06-08 15:02:59 +0900

    PEP8ed

--- tests/httpirucaapi.py
+++ tests/httpirucaapi.py
@@ -1,15 +1,18 @@
 from mytesttool import testerfunc
 import irucapy
 
+
 @testerfunc
 def get_room_info():
     room = api.get_room_info(room_code)
     print(room)
 
+
 @testerfunc
 def get_room_members():
     members = api.get_room_members(room_code)
     print(members)
+
 
 @testerfunc
 def get_room_member():
